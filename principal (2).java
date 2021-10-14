79722b8bf9cb2304e4bcae25686d14e274231ca2
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-14 20:20:13 -0300

    Questão 5

---
+++ principal (2).java
@@ -0,0 +1,13 @@
+public class principal {
+ public static void main (String [] args){
+     contato contato = new nomedocontato;
+     public void enviarmensagemparaocontato(){
+         return enviarmensagemparaocontato;
+    contato.mostrarmensagemenviadas();
+    System.out.println("Digite 5 mensagem");
+    
+    
+     }
+     
+ }
+}
