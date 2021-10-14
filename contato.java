814cb4211e33566bfc82a93c9bd29acbc08e1800
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-14 20:19:04 -0300

    Questão 5

---
+++ contato.java
@@ -0,0 +1,26 @@
+public class contato{
+    String nome;
+    int numero;
+    
+    public produto(String nome, int numero){
+        this.nome = nome;
+        this.numero = numero;
+        pubic String getnome(){
+            return nome;
+        }
+        public void setnome(String nome){
+            this.nome = nome
+        }
+        public float getnumero(){
+            return numero;
+        }
+        public float setnumero(){
+            this.numero = numero;
+        }
+        
+        
+    }
+}
+    
+        
+    }
