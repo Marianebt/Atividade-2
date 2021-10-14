abd09344154ddef65b6d1f4cf312409b10db07ed
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-14 19:50:19 -0300

    Questão 4

---
+++ principal (1).java
@@ -0,0 +1,12 @@
+public class Principal {
+ public static void main (String [] args){
+     produto produto = new produto();
+     produto.mostranome();
+     produto.mostrapreço();
+     produto.comprarumproduto(-1);
+     produto.mostraquantidade();
+     produto.comprarproduto(-9);
+     produto.mostraquantidade;
+     
+ }
+}
