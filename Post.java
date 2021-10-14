2e881804ef9057d602e680bbe472336bc62d4998
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-14 09:44:10 -0300

    Questão 2

---
+++ Post.java
@@ -0,0 +1,21 @@
+public class Post {
+   public String texto;
+   public String link;
+   public int numeroCurtidas;
+   public int numeroCompartilhamento;
+    public String gettexto(){
+        return this.texto;
+    public String getlink(){
+        return this.link;
+    public int getnumeroCurtidas(){
+        return this.numeroCurtidas;
+    public int getnumeroCompartilhamento(){
+        return this.numeroCompartilhamento;
+    }
+    }
+    }
+        
+    }
+    
+}   
+   
