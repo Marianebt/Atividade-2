c067c1ffcd3e4c9112c6bf68be82ac42b2cbabcd
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-14 10:37:29 -0300

    Questão 3

---
+++ Aviao.java
@@ -0,0 +1,31 @@
+public class Aviao {
+    private int altitude;
+    private int velocidade;
+    public Aviao(int altitude, int velocidade){
+        this.altitude= altitude;
+        this.velocidade = velocidade;
+        
+        public int getAtitude(){
+            return altitude;
+        public void setAltitude(int altitude){
+            this.altitude;
+        public void setAltitude(int altitude){
+            this.altitude = altitude;
+        
+        }
+        }
+        }
+    }
+    
+}
+
+        public int getVelocidade(){
+            return velocidade;
+        public void setVelocidade(int velocidade){
+            this.velocidade
+        public void setVelocidade(int velocidade){
+            this.velocidade = velocidade;
+        }
+        }
+        }
+   
